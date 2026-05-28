f7c8a8959147762c3784e203fe256da0d6761a8d
SantiagoLeonE <santiago.leone@uqvirtual.edu.co>
2026-05-27 23:29:30 -0500

    fix: Se agregó la dirección donde está el frontend desplegado en el archivo CorsConfig para poder permitir la comunicación entre ambas partes

--- GestionSolicitudesAcademicas/src/main/java/co/edu/uniquindio/gestionacademica/config/CorsConfig.java
+++ GestionSolicitudesAcademicas/src/main/java/co/edu/uniquindio/gestionacademica/config/CorsConfig.java
@@ -21,7 +21,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         //Frontend permitido
-        config.setAllowedOriginPatterns(List.of(allowedOrigins));
+        config.setAllowedOriginPatterns(List.of(allowedOrigins, "https://proyectofinalprogavanzadafrontend.onrender.com"));
 
         //Métodos HTTP permitidos
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
